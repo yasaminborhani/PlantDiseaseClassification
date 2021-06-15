7ca57757af719be005755b1d211d0d151be7e134
JVD9kh96 <J.khorramdel96@gmail.com>
2021-06-15 10:52:25 +0000

    template files are added

---
+++ networks.py
@@ -0,0 +1,9 @@
+import os
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+import tensorflow.keras.backend as K
