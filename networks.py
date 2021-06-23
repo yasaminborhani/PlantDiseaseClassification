052c19cbabe7b62ba8b77b74217e474bca8baabe
yasaminborhani <borhaniyasamin@gmail.com>
2021-06-23 21:27:48 +0000

    model 6 has been fixed

--- networks.py
+++ networks.py
@@ -231,36 +231,36 @@ def model_maker(target_size, model_id, num_classes = 3):
       cnv4 = Conv2D(filters = 16, kernel_size = (3, 3), strides = (1, 1), name = 'conv_4')(cnv3)
       mxp2 = MaxPooling2D(pool_size = (2, 2), strides= (2, 2), name = 'maxpool_2')(cnv4)
       size_mxp2 = getattr(mxp2, 'shape')
-        image_size =size_mxp2[1]  # We'll resize input images to this size
-        patch_size = 10  # Size of the patches to be extract from the input images
-        num_patches = (image_size // patch_size) ** 2
-        projection_dim = 64
-        num_heads = 4
-        transformer_units = [
-            projection_dim * 2,
-            projection_dim]  # Size of the transformer layers
-        transformer_layers = 2
-        mlp_head_units = [50, 50]
-        patches = Patches(patch_size)(mxp1)
-        # Encode patches.
-        encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+      image_size =size_mxp2[1]  # We'll resize input images to this size
+      patch_size = 10  # Size of the patches to be extract from the input images
+      num_patches = (image_size // patch_size) ** 2
+      projection_dim = 64
+      num_heads = 4
+      transformer_units = [
+          projection_dim * 2,
+          projection_dim]  # Size of the transformer layers
+      transformer_layers = 2
+      mlp_head_units = [50, 50]
+      patches = Patches(patch_size)(mxp1)
+      # Encode patches.
+      encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
 
-        # Create multiple layers of the Transformer block.
-        for _ in range(transformer_layers):
-            # Layer normalization 1.
-            x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-            # Create a multi-head attention layer.
-            attention_output = layers.MultiHeadAttention(
-                num_heads=num_heads, key_dim=projection_dim, dropout=0.1
-            )(x1, x1)
-            # Skip connection 1.
-            x2 = layers.Add()([attention_output, encoded_patches])
-            # Layer normalization 2.
-            x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-            # MLP.
-            x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-            # Skip connection 2.
-            encoded_patches = layers.Add()([x3, x2])
+      # Create multiple layers of the Transformer block.
+      for _ in range(transformer_layers):
+          # Layer normalization 1.
+          x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+          # Create a multi-head attention layer.
+          attention_output = layers.MultiHeadAttention(
+              num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+          )(x1, x1)
+          # Skip connection 1.
+          x2 = layers.Add()([attention_output, encoded_patches])
+          # Layer normalization 2.
+          x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+          # MLP.
+          x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+          # Skip connection 2.
+          encoded_patches = layers.Add()([x3, x2])
 
         # Create a [batch_size, projection_dim] tensor.
         representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
